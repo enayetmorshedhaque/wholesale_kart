5c224e9f51a746bc9e62be267cecad6552065dfa
Enayet Morshed Haque <enayet4419@gmail.com>
2023-06-15 18:19:33 +0600

    added auto generate of order and invoice number feature.

---
+++ assets/db/autogeneratenumber.php
@@ -0,0 +1,44 @@
+<?php
+require('connection.php');
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    if (isset($_REQUEST['check_increment_order_number'])) {
+        $query = "SELECT order_no FROM `orders_table` ORDER BY order_no DESC";
+        $result = mysqli_query($conn, $query);
+
+        $row = mysqli_fetch_array($result);
+
+        if (empty($row['order_no'])) {
+            $number = "BB-0000001-HO";
+            echo $number;
+        } else {
+            $orderId = str_replace("INV-BB-", "", $row['order_no']);
+            $id = str_pad(($orderId + 1), 7, 0, STR_PAD_LEFT);
+            $number = "BB-" . $id . "-HO";
+            echo $number;
+        }
+    }
+
+    if (isset($_REQUEST['check_increment_invoice_number'])) {
+        $query = "SELECT invoice_no FROM `invoice_table` ORDER BY invoice_no DESC";
+        $result = mysqli_query($conn, $query);
+
+        $row = mysqli_fetch_array($result);
+
+        if (empty($row['invoice_no'])) {
+            $number = "INV-BB-0000001";
+            echo $number;
+        } else {
+            $invoiceId = str_replace("INV-BB-", "", $row['invoice_no']);
+            $id = str_pad(($invoiceId + 1), 7, 0, STR_PAD_LEFT);
+            $number = "INV-BB-" . $id;
+            echo $number;
+        }
+    }
+
+}
+mysqli_close($conn);
+?>
