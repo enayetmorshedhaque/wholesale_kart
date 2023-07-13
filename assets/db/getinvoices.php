1e5197779623b3678ecd29e2afc485783eb3835f
Enayet Morshed Haque <enayet4419@gmail.com>
2023-07-14 00:06:44 +0600

    working on invoice list pagination.

--- assets/db/getinvoices.php
+++ assets/db/getinvoices.php
@@ -6,7 +6,22 @@
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     } else{
-        $sql = "SELECT * FROM invoices_table";
+        $sql = "SELECT 
+                    orders_table.order_id, invoice_date, invoice_no, customer_name, total_bill, payment_method
+                FROM
+                    orders_table,
+                    invoices_table,
+                    customers_table,
+                    billing_details_table,
+                    payment_details_table
+                WHERE 
+                    orders_table.order_id = invoices_table.order_id
+                    AND customers_table.order_id = invoices_table.order_id
+                    AND billing_details_table.order_id = invoices_table.order_id
+                    AND payment_details_table.order_id = orders_table.order_id
+                ";
         $result = mysqli_query($conn, $sql);
+
+        
     }
 ?>
