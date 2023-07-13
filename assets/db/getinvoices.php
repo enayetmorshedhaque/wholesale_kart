a36c41f6fc7ecd78597361cab5bb19f7932a42c6
Enayet Morshed Haque <enayet4419@gmail.com>
2023-07-14 01:26:43 +0600

    added dynamic pagination on invoice-list.

--- assets/db/getinvoices.php
+++ assets/db/getinvoices.php
@@ -22,6 +22,42 @@
                 ";
         $result = mysqli_query($conn, $sql);
 
-        
+        if(isset($_REQUEST['pages'])){
+            $pages = $_REQUEST['pages'];
+            $pages = mysqli_real_escape_string($conn, $pages);
+            $pages = htmlentities($pages);
+        }else{
+            $pages = 1;
+        }
+
+        $count = mysqli_num_rows($result);
+        $per_page = 1;
+        $no_of_pages = ceil($count/$per_page);
+        $previous = $no_of_pages - 1;
+        $next = $pages + 1;
+        $start = ($pages - 1) * $per_page;
+        $sql = "SELECT 
+                    orders_table.order_id, invoice_date, invoice_no, customer_name, total_bill, payment_method
+                FROM
+                    orders_table,
+                    invoices_table,
+                    customers_table,
+                    billing_details_table,
+                    payment_details_table
+                WHERE 
+                    orders_table.order_id = invoices_table.order_id
+                    AND customers_table.order_id = invoices_table.order_id
+                    AND billing_details_table.order_id = invoices_table.order_id
+                    AND payment_details_table.order_id = orders_table.order_id
+                
+                LIMIT $start, $per_page
+                ";
+            $result = mysqli_query($conn, $sql);
+
+            if(mysqli_num_rows($result) > 0){
+
+            }else{
+                header("Location: invoice-list.html/pages=1");
+            }
     }
 ?>
