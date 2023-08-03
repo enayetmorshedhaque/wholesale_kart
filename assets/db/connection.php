729c52336891e157fac9b41355d8effb78393dbd
Enayet Morshed Haque <enayet4419@gmail.com>
2023-08-04 02:37:16 +0600

    first commit

--- assets/db/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "booksbd_invoices";
-
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-?>
