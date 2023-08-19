6b7604a44fbbcf68942dd7c8ee57699d77d2ad65
Enayet Morshed Haque <enayet4419@gmail.com>
2023-08-19 13:23:33 +0600

    added dynamic settings option & ui

---
+++ assets/db/connection.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "wholesale_kart";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
---
+++ assets/db/dynamic-settings-data-submit.php
@@ -0,0 +1,13 @@
+<?php
+// require 'connection.php';
+
+// Check connection
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// } else {
+//     if (isset($_REQUEST['mfsTypeAdd'])) {
+//         $add_mfs_type = $_REQUEST['add_mfs_type'];
+//         echo $add_mfs_type;
+//     }
+// }
+// mysqli_close($conn);
