4149e9d8f95ef81e697536c8b17d026311710ce0
Enayet Morshed Haque <enayet4419@gmail.com>
2023-08-29 20:04:24 +0600

    so far done with add supplier form validation

--- assets/js/custom-js/from.validation.js
+++ assets/js/custom-js/from.validation.js
@@ -44,7 +44,6 @@ $(document).ready(function () {
     $("#supplier_name_english").on("blur", function () {
         let bengaliCharacterPattern = /^[\u0980-\u09FF\s]+$/;
         let bengaliNumberPattern = /^[\u09E6-\u09EF]+$/;
-        let englishCharacterPattern = /^[a-zA-Z\s]+$/;
         let numbers = /([0-9])/;
         let specialCharacters = /[^\w\s]/gi;
 
@@ -148,6 +147,19 @@ $(document).ready(function () {
         } else {
             $("#supplier_mfs_type_feedback").html("").removeClass("valid-feedback");
             $("#supplier_mfs_type").removeClass("is-valid");
+
+            let supplier_mfs_number = $("#supplier_mfs_number").val();
+            let supplier_mfs_service = $("#supplier_mfs_service").val();
+
+            if (supplier_mfs_number.length <= 0) {
+                $("#supplier_mfs_number_feedback").html("").removeClass("valid-feedback invalid-feedback");
+                $("#supplier_mfs_number").removeClass("is-valid is-invalid");
+            }
+
+            if (supplier_mfs_service.length <= 0) {
+                $("#supplier_mfs_service_feedback").html("").removeClass("valid-feedback invalid-feedback");
+                $("#supplier_mfs_service").removeClass("is-valid is-invalid");
+            }
         }
     });
 
@@ -157,6 +169,19 @@ $(document).ready(function () {
         if (($(this).val()).length <= 0) {
             $("#supplier_mfs_number_feedback").html("").removeClass("valid-feedback");
             $("#supplier_mfs_number").removeClass("is-valid");
+
+            let supplier_mfs_service = $("#supplier_mfs_service").val();
+            let supplier_mfs_type = $("#supplier_mfs_type").val();
+
+            if (supplier_mfs_type.length <= 0) {
+                $("#supplier_mfs_type_feedback").html("").removeClass("valid-feedback invalid-feedback");
+                $("#supplier_mfs_type").removeClass("is-valid is-invalid");
+            }
+
+            if (supplier_mfs_service.length <= 0) {
+                $("#supplier_mfs_service_feedback").html("").removeClass("valid-feedback invalid-feedback");
+                $("#supplier_mfs_service").removeClass("is-valid is-invalid");
+            }
         } else if (($(this).val()).length < 11 || ($(this).val()).length > 12 || ($(this).val()).charAt(0).match(phone_number_validation)) {
             $("#supplier_mfs_number_feedback").html("Enter Valid MFS Number.").removeClass("valid-feedback").addClass("invalid-feedback");
             $("#supplier_mfs_number").removeClass("is-valid").addClass("is-invalid");
@@ -173,6 +198,19 @@ $(document).ready(function () {
         } else {
             $("#supplier_mfs_service_feedback").html("").removeClass("valid-feedback");
             $("#supplier_mfs_service").removeClass("is-valid");
+
+            let supplier_mfs_number = $("#supplier_mfs_number").val();
+            let supplier_mfs_type = $("#supplier_mfs_type").val();
+
+            if (supplier_mfs_type.length <= 0) {
+                $("#supplier_mfs_type_feedback").html("").removeClass("valid-feedback invalid-feedback");
+                $("#supplier_mfs_type").removeClass("is-valid is-invalid");
+            }
+
+            if (supplier_mfs_number.length <= 0) {
+                $("#supplier_mfs_number_feedback").html("").removeClass("valid-feedback invalid-feedback");
+                $("#supplier_mfs_number").removeClass("is-valid is-invalid");
+            }
         }
     });
 
@@ -181,8 +219,21 @@ $(document).ready(function () {
             $("#supplier_bank_account_no_feedback").html("Looks Good!").removeClass("invalid-feedback").addClass("valid-feedback");
             $("#supplier_bank_account_no").removeClass("is-invalid").addClass("is-valid");
         } else {
-            $("#supplier_bank_account_no_feedback").html("").removeClass("valid-feedback");
-            $("#supplier_bank_account_no").removeClass("is-valid");
+            $("#supplier_bank_account_no_feedback").html("").removeClass("valid-feedback invalid-feedback");
+            $("#supplier_bank_account_no").removeClass("is-valid is-invalid");
+
+            let supplier_bank_name = $("#supplier_bank_name").val();
+            let supplier_bank_branch_name = $("#supplier_bank_branch_name").val();
+
+            if (supplier_bank_name.length <= 0) {
+                $("#supplier_bank_name_feedback").html("").removeClass("valid-feedback invalid-feedback");
+                $("#supplier_bank_name").removeClass("is-valid is-invalid");
+            }
+
+            if (supplier_bank_branch_name.length <= 0) {
+                $("#supplier_bank_branch_name_feedback").html("").removeClass("valid-feedback invalid-feedback");
+                $("#supplier_bank_branch_name").removeClass("is-valid is-invalid");
+            }
         }
     });
 
@@ -191,8 +242,21 @@ $(document).ready(function () {
             $("#supplier_bank_name_feedback").html("Looks Good!").removeClass("invalid-feedback").addClass("valid-feedback");
             $("#supplier_bank_name").removeClass("is-invalid").addClass("is-valid");
         } else {
-            $("#supplier_bank_name_feedback").html("").removeClass("valid-feedback");
-            $("#supplier_bank_name").removeClass("is-valid");
+            $("#supplier_bank_name_feedback").html("").removeClass("valid-feedback invalid-feedback");
+            $("#supplier_bank_name").removeClass("is-valid is-invalid");
+
+            let supplier_bank_account_no = $("#supplier_bank_account_no").val();
+            let supplier_bank_branch_name = $("#supplier_bank_branch_name").val();
+
+            if (supplier_bank_account_no.length <= 0) {
+                $("#supplier_bank_account_no_feedback").html("").removeClass("valid-feedback invalid-feedback");
+                $("#supplier_bank_account_no").removeClass("is-valid is-invalid");
+            }
+
+            if (supplier_bank_branch_name.length <= 0) {
+                $("#supplier_bank_branch_name_feedback").html("").removeClass("valid-feedback invalid-feedback");
+                $("#supplier_bank_branch_name").removeClass("is-valid is-invalid");
+            }
         }
     });
 
@@ -201,8 +265,8 @@ $(document).ready(function () {
             $("#supplier_bank_branch_name_feedback").html("Looks Good!").removeClass("invalid-feedback").addClass("valid-feedback");
             $("#supplier_bank_branch_name").removeClass("is-invalid").addClass("is-valid");
         } else {
-            $("#supplier_bank_branch_name_feedback").html("").removeClass("valid-feedback");
-            $("#supplier_bank_branch_name").removeClass("is-valid");
+            $("#supplier_bank_branch_name_feedback").html("").removeClass("valid-feedback invalid-feedback");
+            $("#supplier_bank_branch_name").removeClass("is-valid is-invalid");
         }
     });
 
@@ -245,6 +309,11 @@ $(document).ready(function () {
         let supplier_address = $("#supplier_address").val();
         let supplier_contact = $("#supplier_contact").val();
 
+        // MFS Information
+        let supplier_mfs_service = $("#supplier_mfs_service").val();
+        let supplier_mfs_number = $("#supplier_mfs_number").val();
+        let supplier_mfs_type = $("#supplier_mfs_type").val();
+
         // Bank Information
         let supplier_bank_account_no = $("#supplier_bank_account_no").val();
         let supplier_bank_name = $("#supplier_bank_name").val();
@@ -254,6 +323,7 @@ $(document).ready(function () {
         let supplier_social_contact_number = $("#supplier_social_contact_number").val();
         let supplier_social_contact_service = $("#supplier_social_contact_service").val();
 
+        // ===========================================================================================================
 
         // Business Information Length
         let supplier_name_bengali_input_length = supplier_name_bangla.length;
@@ -261,12 +331,17 @@ $(document).ready(function () {
         let supplier_address_input_length = supplier_address.length;
         let supplier_contact_input_length = supplier_contact.length;
 
+        // MFS Information
+        let supplier_mfs_service_input_length = supplier_mfs_service.length;
+        let supplier_mfs_number_input_length = supplier_mfs_number.length;
+        let supplier_mfs_type_input_length = supplier_mfs_type.length;
+
         // Bank Information Length
         let supplier_bank_account_no_input_length = supplier_bank_account_no.length;
         let supplier_bank_name_input_length = supplier_bank_name.length;
         let supplier_bank_branch_name_input_length = supplier_bank_branch_name.length;
 
-        // Bank Information Length
+        // Social Contact Length
         let supplier_social_contact_number_input_length = supplier_social_contact_number.length;
         let supplier_social_contact_service_input_length = supplier_social_contact_service.length;
 
@@ -293,6 +368,51 @@ $(document).ready(function () {
         if (supplier_contact_input_length <= 0) {
             $("#supplier_contact_feedback").html("This field is required.").removeClass("valid-feedback").addClass("invalid-feedback");
             $("#supplier_contact").removeClass("is-valid").addClass("is-invalid");
+            isValid = false;
+        }
+
+        if (supplier_mfs_service_input_length > 0 && supplier_mfs_number_input_length <= 0 && supplier_mfs_type_input_length <= 0) {
+            $("#supplier_mfs_number_feedback").html("This field is required.").removeClass("valid-feedback").addClass("invalid-feedback");
+            $("#supplier_mfs_number").removeClass("is-valid").addClass("is-invalid");
+
+            $("#supplier_mfs_type_feedback").html("This field is required.").removeClass("valid-feedback").addClass("invalid-feedback");
+            $("#supplier_mfs_type").removeClass("is-valid").addClass("is-invalid");
+            isValid = false;
+        }
+
+        if (supplier_mfs_number_input_length > 0 && supplier_mfs_type_input_length <= 0 && supplier_mfs_service_input_length <= 0) {
+            $("#supplier_mfs_type_feedback").html("This field is required.").removeClass("valid-feedback").addClass("invalid-feedback");
+            $("#supplier_mfs_type").removeClass("is-valid").addClass("is-invalid");
+
+            $("#supplier_mfs_service_feedback").html("This field is required.").removeClass("valid-feedback").addClass("invalid-feedback");
+            $("#supplier_mfs_service").removeClass("is-valid").addClass("is-invalid");
+            isValid = false;
+        }
+
+        if (supplier_mfs_type_input_length > 0 && supplier_mfs_service_input_length <= 0 && supplier_mfs_number_input_length <= 0) {
+            $("#supplier_mfs_number_feedback").html("This field is required.").removeClass("valid-feedback").addClass("invalid-feedback");
+            $("#supplier_mfs_number").removeClass("is-valid").addClass("is-invalid");
+
+            $("#supplier_mfs_service_feedback").html("This field is required.").removeClass("valid-feedback").addClass("invalid-feedback");
+            $("#supplier_mfs_service").removeClass("is-valid").addClass("is-invalid");
+            isValid = false;
+        }
+
+        if (supplier_mfs_service_input_length > 0 && supplier_mfs_number_input_length > 0 && supplier_mfs_type_input_length <= 0) {
+            $("#supplier_mfs_type_feedback").html("This field is required.").removeClass("valid-feedback").addClass("invalid-feedback");
+            $("#supplier_mfs_type").removeClass("is-valid").addClass("is-invalid");
+            isValid = false;
+        }
+
+        if (supplier_mfs_number_input_length > 0 && supplier_mfs_type_input_length > 0 && supplier_mfs_service_input_length <= 0) {
+            $("#supplier_mfs_service_feedback").html("This field is required.").removeClass("valid-feedback").addClass("invalid-feedback");
+            $("#supplier_mfs_service").removeClass("is-valid").addClass("is-invalid");
+            isValid = false;
+        }
+
+        if (supplier_mfs_type_input_length > 0 && supplier_mfs_service_input_length > 0 && supplier_mfs_number_input_length <= 0) {
+            $("#supplier_mfs_number_feedback").html("This field is required.").removeClass("valid-feedback").addClass("invalid-feedback");
+            $("#supplier_mfs_number").removeClass("is-valid").addClass("is-invalid");
             isValid = false;
         }
 
